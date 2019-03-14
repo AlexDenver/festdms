7c2712047681e4ca01327163697da0f708fe8697
Alex Denver <dnvr.dsz@gmail.com>
2019-03-14 10:45:39 +0530

    AB6 Secure Test 3

--- client/imports/app/roleguard.services.ts
+++ client/imports/app/roleguard.services.ts
@@ -4,7 +4,7 @@ import { Meteor } from 'meteor/meteor';
 
 import {MongoObservable, MeteorObservable, zoneOperator, ObservableCursor} from 'meteor-rxjs';
 import { Observable, Subscription  } from 'rxjs';
-import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import {Roles} from "meteor/alanning:roles";
 
 
@@ -14,11 +14,21 @@ import {Roles} from "meteor/alanning:roles";
   })
   export class RoleGuard implements OnInit{
 
+    constructor(private router: Router){
 
+    }
     ngOnInit(){
         
     }
+    resolve(state: RouterStateSnapshot): Observable {
+      let d = Meteor.user();
+      if(d){
+        return d;
+      }else{
+        this.router.navigate(['auth']);
+      }
 
+    }
     canActivate(route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot){
         let roles = route.data["roles"] as Array<string>;
